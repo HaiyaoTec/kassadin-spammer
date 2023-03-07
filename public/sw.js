b9c5efcb74d03092ceb2c10ce542ed1eb5586112
hyming <a13308263216@gmail.com>
2023-03-07 16:36:06 +0800

    fix: web app

--- public/sw.js
+++ public/sw.js
@@ -4,7 +4,8 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(cacheName)
     .then(cache => cache.addAll([
-      '/'
+      '/',
+      '/login'
     ]))
   )
 })
