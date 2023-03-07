d7829f171ed20a21b6b739e612de1fdb94bf155d
hyming <a13308263216@gmail.com>
2023-03-07 16:57:05 +0800

    fix: web app no cache

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,14 @@
 var cacheName = 'EearningRhino'
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(cacheName)
-    .then(cache => cache.addAll([
-      '/',
-      '/login'
-    ]))
-  )
+  self.skipWaiting()
+  // event.waitUntil(
+  //   caches.open(cacheName)
+  //   .then(cache => cache.addAll([
+  //     '/',
+  //     '/login'
+  //   ]))
+  // )
 })
 
 self.addEventListener('fetch', function (event) {
