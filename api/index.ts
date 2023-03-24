1fcf7a12809156e7ece0f5a46de203f1ccf0d8cf
silence-may <a13308263216@gmail.com>
2023-03-24 16:41:50 +0800

    path

--- api/index.ts
+++ api/index.ts
@@ -4,7 +4,7 @@ import type { AxiosRequestConfig, AxiosResponse } from 'axios'
 // 处理查询参数为数组的情况
 import * as qs from 'qs'
 // 配置默认baseURL
-const curBaseURL = 'https://api.earningrhino.com/api/'
+const curBaseURL = 'https://samira.jinuo.fun/api/'
 import {Api as ServicePayApi} from './samira-service-proxyApi'
 import {Api as HeraclesPayApi} from './heracles-payApi'
 // @ts-ignore
