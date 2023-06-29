1a3159a986780c19af4528ba49b29f7b8a7e5c55
silence-may <a13308263216@gmail.com>
2023-06-29 17:51:37 +0800

    fix

--- api/errorCode.tsx
+++ api/errorCode.tsx
@@ -1,5 +1,5 @@
 export const errorCode:Record<string, string> = {
-    '1005':"Login Error",
+    '1005':"Gagal masuk",
     '1006':"UID tidak ada",
     '1007':"Pengguna telah bergabung dengan promosi",
     '1008':"Spammer Invalid Param",
