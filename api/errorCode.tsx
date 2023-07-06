c19ee9987c8b72b7dc95a4aa5e7c2995bce22641
silence-may <a13308263216@gmail.com>
2023-07-06 18:18:47 +0800

    fix errcode

--- api/errorCode.tsx
+++ api/errorCode.tsx
@@ -5,6 +5,9 @@ export const errorCode:Record<string, string> = {
     '1008':"Spammer Invalid Param",
     '1009': "Spammer Invalid UID",
     '1010': "Spammer Invalid WA",
+    '1011':'Daftar 7 Hari yang lalu',
+    '1012':'Deposit Pertama di bawah minimum',
+    '1013':'Tidak ada Deposit',
     '-1':"Kesalahan server",
     '-2':"Kerusakan jaringan terjadi nanti"
 }
